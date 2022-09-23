44e1ae2c36b5d5109ee59b6b01584eae22b90686
ryoo14 <anana12185@gmail.com>
2022-09-23 22:25:05 +0900

    initial commit

---
+++ main.tsx
@@ -0,0 +1,22 @@
+/** @jsx h */
+
+import blog, { ga, h } from "blog";
+
+blog({
+  title: "/var/log/ryoo14",
+  description: "ざっき",
+  // header: <header>Your custom header</header>,
+  // section: <section>Your custom section</section>,
+  // footer: <footer>Your custom footer</footer>,
+  avatar: "/ryoo.svg",
+  avatarClass: "rounded-full",
+  author: "ryoo14",
+  links: [
+    { title: "GitHub", url: "https://github.com/ryoo14" },
+    { title: "HOME", url: "https://uukk.life" },
+  ],
+  favicon: "/ry.svg",
+  middlewares: [
+    ga("UA-47086708-1"),
+  ],
+});
